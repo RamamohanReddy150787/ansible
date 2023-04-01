dbf6eeb4d37a3f4228784e1ec8598bc6fd761652
root <root@ip-172-31-13-200.ec2.internal>
2023-04-01 09:35:48 +0000

    updated script

--- scripts/useradd.sh
+++ scripts/useradd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in `cat /tmp/userdetails/users/users_list`
+for i in `cat /tmp/userdetails12345/users/users_list`
 do
 id $i
 if [ $? -eq 0 ]
@@ -9,7 +9,7 @@ else
 useradd $i
 fi
 mkdir /home/$i/.ssh
-cp /tmp/userdetails/keys/$i.keys /home/$i/.ssh/authorized_keys
+cp /tmp/userdetails12345/keys/$i.keys /home/$i/.ssh/authorized_keys
 chmod 400 /home/$i/.ssh/authorized_keys
 chmod 600 /home/$i/.ssh
 chown -R $i:$i /home/$i/.ssh
